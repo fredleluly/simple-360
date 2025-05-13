f228a68b3b87e8c7480384614a57d7c2368243d3
fredleluly <fredleluly@gmail.com>
2025-05-13 11:41:34 +0700

    f

---
+++ data.js
@@ -0,0 +1,80 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-salinan-img_20220108_134923_00_0321",
+      "name": "Salinan IMG_20220108_134923_00_032(1)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -2.9664680851477314,
+        "pitch": 0.021319649592456358,
+        "fov": 1.3401592523986738
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.8328589653083363,
+          "pitch": 0.0667651937544349,
+          "rotation": 0,
+          "target": "1-salinan-img_20220108_135304_00_036"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-salinan-img_20220108_135304_00_036",
+      "name": "Salinan IMG_20220108_135304_00_036",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
